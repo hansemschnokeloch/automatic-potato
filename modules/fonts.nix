e2ceea4d1a6f3b44bdbeb0a27481b5bf49a0163f
pascal <sarl@hubrecht.info>
2023-07-09 15:31:01 +0200

    update fonts

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -1,19 +1,33 @@
 { pkgs, ... }:
+
+let
+  unstable = import <nixos-unstable> {
+    config = {
+      allowUnfree = true;
+    };
+  };
+in
+
 {
   fonts.fonts = with pkgs; [
     # https://nixos.wiki/wiki/Fonts
-    (nerdfonts.override { fonts = [ "FiraCode" "DejaVuSansMono" ]; })
-    carlito
+    # (nerdfonts.override { fonts = [ "FiraCode" "DejaVuSansMono" ]; })
+    # carlito
+    # fira
+    # fira-code
+    # font-awesome
+    # libertine
+    # powerline-fonts
+    # source-sans-pro
+    # source-serif-pro
+    # stix-two
+    # vistafonts
+    (unstable.nerdfonts.override { fonts = [ "FiraCode" "SourceCodePro" ]; })
     fira
     fira-code
-    font-awesome
     libertine
-    powerline-fonts
-    source-sans-pro
     source-serif-pro
     stix-two
     vistafonts
   ];
 }
-
-
