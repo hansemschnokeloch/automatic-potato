0d84616fea3c53251e52c6b81f4e826164ffec41
pascal <sarl@hubrecht.info>
2023-07-09 15:31:20 +0200

    add rpiplay ios

--- modules/ios.nix
+++ modules/ios.nix
@@ -10,6 +10,10 @@ lib.mkIf (config.networking.hostName == "ph-thinkstation")
   environment.systemPackages = with pkgs; [
     libimobiledevice
     ifuse # optional, to mount using 'ifuse'
+    rpiplay
   ];
 
+  # ports for rpiplay
+  networking.firewall.allowedTCPPorts = [ 7000 7100 ];
+  networking.firewall.allowedUDPPorts = [ 6000 6001 7011 ];
 }
