f12d44907764235de2304a555a624409ef78e1f7
pascal <sarl@hubrecht.info>
2023-07-09 15:32:13 +0200

    update packages

--- modules/packages.nix
+++ modules/packages.nix
@@ -7,21 +7,26 @@ let
     };
   };
   navicat = pkgs.callPackage ./../pkgs/navicat.nix { };
+  python = python39.withPackages (ps: with ps; [ pynvim msgpack flake8 ]);
 in
 
 {
   nixpkgs.overlays = [ (import ./../overlays/insomnia.nix) ];
+  # configuration
+  programs.firefox = {
+    enable = true;
+    languagePacks = [ "fr" "en-US" ];
+  };
   environment.systemPackages = [
     # essential
     chromium # web browser
     exa # replacement for ls
     fd # simple alternative to find
     file # determine type of file
-    firefox # web browser
     fzf # command line fuzzy finder
     gthumb # image viewer
     jq # command line json processor
-    unstable.kitty # terminal
+    kitty # terminal
     rar # utility for rar archives
     ripgrep # (rg) recursive regex search utility
     starship # customizable shell prompt
@@ -43,8 +48,9 @@ in
     # webdev
     navicat
     postman # API development environment
-    httpie
-    insomnia
+    # httpie # API development environment
+    insomnia # API development environment
+    wkhtmltopdf-bin
 
     # dev
     gcc # GNU compiler collection
@@ -54,30 +60,27 @@ in
 
     # utils
     dig # domain name server
-    docker-compose # compose for docker
+    # docker-compose # compose for docker
     kooha # screen recorder
     lm_sensors # read hardware sensors
     neofetch # system info script
     poppler_utils # pdf rendering library
     protonvpn-gui # GUI for proton VPN
     timewarrior # time tracking
-    virtualbox # virtualization
+    # virtualbox # virtualization
 
     # node
     nodejs
     nodePackages.prettier
     nodePackages.neovim
     nodePackages.intelephense
-    nodePackages.tailwindcss
     nodePackages.eslint
 
-    # py3
-    python3
-    python3Packages.pynvim # Python Package Required for neovim
-    python3Packages.msgpack # Python Package Required for neovim
-    python3Packages.flake8 # Python Package
+    # python3
+    python
 
-    # latex
+    # latex - pdf
+    masterpdfeditor # pdf editor
     tectonic
     tikzit
 
@@ -86,7 +89,6 @@ in
 
     # office
     libreoffice-fresh # office suite
-    masterpdfeditor # pdf editor
     onlyoffice-bin # office suite
 
   ];
